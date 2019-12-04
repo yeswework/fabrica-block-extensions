fbc56f21d201a132c9e88f9172d97b0eb28b9fc5
Andrew Staffell <andrew@yeswework.com>
2019-12-04 17:48:35 +0100

    allow callback function to return conditional data

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const extensions = {};
 
-const extendBlockEdit = (BlockEdit) => (
-	(props) => wp.element.createElement(BlockEdit, {...props, extensions: extensions[props.name]})
-);
+const extendBlockEdit = BlockEdit => props => wp.element.createElement(BlockEdit, !extensions[props.name] ? props : {...props, extensions: extensions[props.name](wp)});
 wp.hooks.addFilter('editor.BlockEdit', 'fabrica/extend-blocks', extendBlockEdit);
 
-export default function registerBlockExtensions(contexts, data) {
+export default function registerBlockExtensions(contexts, extensionsData) {
 	if (typeof contexts === 'string') { contexts = [contexts]; }
 	if (typeof contexts !== 'object') { return false; }
-	contexts.forEach(context => extensions[context] = data);
+	const getExtensions = typeof extensionsData === 'function' ? extensionsData : () => extensionsData;
+	contexts.forEach(context => extensions[context] = getExtensions);
 }
